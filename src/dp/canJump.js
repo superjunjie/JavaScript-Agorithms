e905a0d42c609a174322d047ae249d4c49eda4d8
YANJUNJIE <pcxiaoyan@gmail.com>
2022-06-11 14:02:08 +0800

    feat: 动态规划

---
+++ src/dp/canJump.js
@@ -0,0 +1,12 @@
+var canJump = function (nums) {
+    let n = nums.length, mostRight = 0
+    for (let i = 0; i < n; i++) {
+        if (i <= mostRight) {
+            mostRight = Math.max(mostRight, i + nums[i])
+            if (mostRight >= n - 1) {
+                return true
+            }
+        }
+    }
+    return false
+};
