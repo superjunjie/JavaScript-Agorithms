4ff655bf46f7bae2a14441ad57264c523970c18a
YANJUNJIE <pcxiaoyan@gmail.com>
2022-06-11 14:03:05 +0800

    feat: leetcode

--- leetcode/data-structure/binary/index.js
+++
@@ -1,5 +0,0 @@
-const { generateBinarySearchTree } = require('./create')
-const { inOrderTraversal } = require('./traverse')
-
-const root = generateBinarySearchTree([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1])
-console.log(inOrderTraversal(root, 22))
